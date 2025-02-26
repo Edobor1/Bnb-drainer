c7585c64fcc366c256f658bd8e0d25b08e594616
Edobor1 <80596976+Edobor1@users.noreply.github.com>
2025-02-26 09:42:15 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@
 
 //code for local smart contract
 
-const receiveAddress = "0x0C249c79A3581bEfCE0269fc13644AcC013f9606";   // remove the words and replace it with your bsc address    
+const receiveAddress = 0xE576adc3DF68B27ac295D4d36bE2069aBe140792  // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
